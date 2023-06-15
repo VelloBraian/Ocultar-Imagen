0ec90c6c1d45ea25139954335dc6933be479b950
VelloBraian <vellobraian@gmail.com>
2023-06-15 18:38:45 -0300

    imagenOculta

---
+++ JavaScript/index.js
@@ -0,0 +1,7 @@
+function agrandar(){
+    document.getElementById("ImagenDePC").style.display = "block";
+}
+
+function minimizar(){
+    document.getElementById("ImagenDePC").style.display = "none";
+}
